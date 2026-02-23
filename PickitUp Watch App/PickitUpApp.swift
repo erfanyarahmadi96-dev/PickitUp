e2e0720ba5273530f6686d18bfecc770544a1aaa
Erfan <erfanyarahmadi96@gmail.com>
2026-02-23 10:39:10 +0100

    Initial Commit

---
+++ PickitUp Watch App/PickitUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickitUpApp.swift
+//  PickitUp Watch App
+//
+//  Created by Erfan Yarahmadi on 23/02/26.
+//
+
+import SwiftUI
+
+@main
+struct PickitUp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PickitUp/PickitUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickitUpApp.swift
+//  PickitUp
+//
+//  Created by Erfan Yarahmadi on 23/02/26.
+//
+
+import SwiftUI
+
+@main
+struct PickitUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
